2da1980f16f596ae22702efc3b5cda0254183e44
Hack Reactor Student <student@hackreactor.com>
2017-06-03 12:05:55 -0700

    Implemented live-search on search bar keyup

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,10 @@
 var Search = (props) => (
   <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
-    <button onClick= { function() { props.onSearchSubmit($('.form-control').val()); }} 
-            className="btn hidden-sm-down">
+    <input onKeyUp={ function() {
+      props.onSearchSubmit($('.form-control').val());
+    }} 
+      className="form-control" type="text" />
+    <button className="btn hidden-sm-down">
       <span className="glyphicon glyphicon-search"></span>
     </button>
   </div> 
