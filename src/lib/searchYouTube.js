aa57ebf27c0d396c924196769e133b3ce7daa512
Hack Reactor Student <student@hackreactor.com>
2017-06-02 20:14:56 -0700

    Implemented searchYoutube function

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,20 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      part: 'snippet',
+      key: options.key,
+      q: options.query,
+      maxResults: options.max || 5,
+      type: 'video',
+      videoEmbeddable: 'true'
+    },
+    success: function(data) {
+      callback(data);
+    },
+  });
 };
 
 window.searchYouTube = searchYouTube;
