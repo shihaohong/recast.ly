44c21eaeb6059ddd49f0e6aaafe0112a19547d94
Hack Reactor Student <student@hackreactor.com>
2017-06-02 17:30:27 -0700

    Implemented ES6 App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,38 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer video={window.exampleVideoData[0]} />
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={window.exampleVideoData} />
-    </div>
-  </div>
-);
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      videos: window.exampleVideoData,
+      videoInPlayer: window.exampleVideoData[0]
+    };
+  }
+
+
+  onVideoListClick() {
+    this.setState({
+      
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.videoInPlayer} />
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={this.state.videos} />
+        </div>
+      </div>
+    );
+  }
+
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+
+
